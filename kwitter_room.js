7d4fae452da3570e10d98e6b3a0b9bfb1dbc88b9
Yashmitha7 <89140471+Yashmitha7@users.noreply.github.com>
2021-10-07 03:59:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+//Firebase DataBase //
+const firebaseConfig = {
+    apiKey: "AIzaSyCmthTvKbEGd3xB5-jsbUn6Drsep6S-7vE",
+    authDomain: "let-s-chat-app-7cfbf.firebaseapp.com",
+    projectId: "let-s-chat-app-7cfbf",
+    storageBucket: "let-s-chat-app-7cfbf.appspot.com",
+    messagingSenderId: "821918145031",
+    appId: "1:821918145031:web:fe31499c7c883f706328a2",
+    measurementId: "G-LDK9XBFJYM"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
